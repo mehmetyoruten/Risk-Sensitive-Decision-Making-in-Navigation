74e0b9e22123b2d09346f408aa86dd2022ef1817
mehmetyoruten <mehmetyorutenn@gmail.com>
2022-01-08 16:19:42 +0100

    kb update. instructions, practice trials added.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,22 +6,26 @@
 */
 
 var nclicks = 0
-var trial_count = 1
-var num_trials = 20
-var time_limit = 10
+var number_of_moves = 0
+var max_moves = 20
+var time_limit = 2000 // in ms
+var max_practice = 2
 var experimentState = {
   completedScreenCounter : 0
 }
+var n_keypress = 0
+var trial_n = 0
 var timer = null; 
 var background_color = "#FFFFFF";  // set the default background color for animations
-
 var last_tile = {
-  "trial_n": 0,                 
+  "trial_n": 0,
+  "move_n": 0,                 
   "response_time": 0,
   "submitted_x": 1,
   "submitted_y": 1,
   "key"  : 0
 };
+
 
 /*
 ================================================================================
@@ -37,8 +41,7 @@ $(document).ready(function(){
 
   $("#consent__button__agree").click(function() {
     $(".consent").slideUp();    
-    $(".grids").slideDown()
-    Start_New_Trial()
+    $(".instructions").slideDown();
     });
 
   $("#consent__button__disagree").click(function() {
@@ -46,11 +49,38 @@ $(document).ready(function(){
     $(".return-hit").slideDown()
     });
 
+
+  $("#next-button-instructions").click(function() {
+    $(".instructions").slideUp();    
+    $(".grids").slideDown();
+    Start_New_Move()
+    });
+  
+
   $("#return-hit__button").click(function() {
     $(".return-hit").slideUp();    
-    $(".consent").slideDown()
+    $(".consent").slideDown();
     });
 
+  $("#next-button-trials").click(function() {
+    $(".inter-trial").slideUp();
+    $(".grids").slideDown();    
+    Start_New_Move();    
+    });
+
+
+  $("#start-button").click(function() {
+    $(".practice-end").slideUp();
+    $(".grids").slideDown();    
+    Start_New_Move();    
+    });
+
+        
+ /*
+  ================================================================================
+   Mouse Control
+  ================================================================================
+  */
 
   $(".square").click(function() {
     if ($('.square.active').length == 0) {
@@ -87,22 +117,22 @@ $(document).ready(function(){
       $(".warning-no-cell-selected").slideDown()
     }
 
-    else if (($('.square.active').length + $('.square.processed').length)==trial_count+1) {
+    else if (($('.square.active').length + $('.square.processed').length)==number_of_moves+1) {
         // Get data about the trial
         trialEndTimeMs = new Date().getTime();     
         var response_time = (trialEndTimeMs-trialStartTimeMs)/1000
         var submittedX = $('.active').data('x');
         var submittedY = $('.active').data('y');        
-        var last_tile = saveTrial(trial_count, response_time, submittedX, submittedY);
+        var last_tile = saveTrial(number_of_moves, response_time, submittedX, submittedY);
         window.last_tile = last_tile;
         console.log(last_tile);
         Flash_Background_Correct();   
         
         // Start the next trial.
-        trial_count += 1;    
+        number_of_moves += 1;    
         $('.square.active').addClass("processed")
         $('.square.processed').removeClass("active")              
-        Start_New_Trial()      
+        Start_New_Move()      
     }
   });
 
@@ -114,12 +144,13 @@ $(document).ready(function(){
   ================================================================================
   */
 
-  function Start_New_Trial(experimentState) {
+  function Start_New_Move(experimentState) {
     correct_Sol = [];
     chosen_Sol = [];
-    // Count the number of trials
-    $("#trial_count").html("Number of moves: "+trial_count);
-    // $("#trial_count").html("Trial "+(trial_count)+" / "+num_trials);
+    // Count the number of moves
+    $("#progress").html(" &nbsp;&nbsp; Trial: "+ trial_n + "<br>" + " &nbsp;&nbsp; Number of moves: " +number_of_moves);
+    
+    
     //stop current timer
     if (timer){
         clearTimeout(timer);
@@ -128,25 +159,28 @@ $(document).ready(function(){
     // start_timer(); 
     trialStartTimeMs = new Date().getTime();    
 
-    if (trial_count > num_trials) {
+    if (number_of_moves > max_moves) {
         // The experimet is done, conclude the experiment.
         //clearTimeout(timer);
         //load_next_debriefing_silhouettes();
         $(".grids").slideUp();
         $(".debriefing-instructions").slideDown();              
 
-    } else if (trial_count <= num_trials){
+    } else if (number_of_moves <= max_moves){
         // The experiment is not done, display next stimulus.
         // id = load_next_silhouette(); 
         // saveTrial(trialStartTimeMs, id)
         $(".trial-content__submit-button").slideDown();
-    } else if (($('.processed').data('x') === 7) && ($('.processed').data('y')===1)){
-        $(".grids").slideUp();
-        $(".debriefing-instructions").slideDown();  
-    }    
+    }
+    number_of_moves += 1;
+    var [x_pos, y_pos] = Get_Position();
+    console.log(x_pos)
+    console.log(y_pos)    
+    window.x_pos = x_pos
+    window.y_pos = y_pos
   }
 
-  // Calculate the movement range
+  // Calculate the movement range -for mouse control-
   function calc_range(last_tile_x, last_tile_y, target_tile_x, target_tile_y){        
     var x_diff = Math.abs(last_tile_x - target_tile_x);
     var y_diff = Math.abs(last_tile_y - target_tile_y);
@@ -160,20 +194,20 @@ $(document).ready(function(){
 
 
   function start_timer(){
-    console.log("Start timer with " +  time_limit +" s");
-    timer = setTimeout(time_limit_exeeded, time_limit * 1000)
+    console.log("Start timer");
+    timer = new Date().getTime();  
   }
 
   function time_limit_exeeded(){    
-    trial_count += 1;
+    number_of_moves += 1;
     $(".grids").slideUp();
     $(".time_limit").slideDown()
   }
 
 
-  function saveTrial(trial_count, response_time, submittedX, submittedY, key){  
+  function saveTrial(number_of_moves, response_time, submittedX, submittedY, key){  
     saved_response = {
-      "trial_n": trial_count,                 
+      "move_n": number_of_moves,                 
       "response_time": response_time,
       "submitted_x": submittedX,
       "submitted_y": submittedY,
@@ -209,82 +243,142 @@ $(document).ready(function(){
    Keyboard Control
   ================================================================================
   */
+  function Start_New_Trial() {
+    $('.active').removeClass('active');
+    $('.processed').removeClass('processed');
+    $('#start__square').addClass('processed');
+    $('#start__square').addClass('active');
+    window.number_of_moves = 0;
+
+  }
+  
   function Get_Position() {      
+    n_keypress += 1;
     var x_pos = $(".active").data('x');
-    var y_pos = $(".active").data('y');
-    $('.active').removeClass('active')
+    var y_pos = $(".active").data('y');    
     return [x_pos, y_pos]
   }
 
-  function Submit_Response(key) {
-    setTimeout(function(){
-      trialEndTimeMs = new Date().getTime();     
+
+  function Submit_Response(key) {    
+      // Save the response      
+      
+      trialEndTimeMs = new Date().getTime();           
+      var n_keypress = 0
       var response_time = (trialEndTimeMs-trialStartTimeMs)/1000
       var submittedX = $('.active').data('x');
       var submittedY = $('.active').data('y');        
-      var last_tile = saveTrial(trial_count, response_time, submittedX, submittedY, key);
+      var last_tile = saveTrial(number_of_moves, response_time, submittedX, submittedY, key);
       window.last_tile = last_tile;
-      console.log(last_tile);
-      Flash_Background_Correct();   
-      
-      // Start the next trial.
-      trial_count += 1;    
-      $('.processed').removeClass('processed')
-      $('.active').addClass('processed')      
-      Start_New_Trial()           
-    }, 2000);
+      console.log(last_tile);      
+
+      // Check if the target is reached
+      if (($(".active").data('x')=== $('#end__square').data('x') && ($(".active").data('y') === $('#end__square').data('y')))) {   
+        
+        Flash_Background_Correct();                                                     
+        trial_n +=1;
+        $(".grids").slideUp();    
+        
+        if (trial_n === max_practice) {
+          $(".practice-end").slideDown();             
+        }
+        else {
+          $(".inter-trial").slideDown();             
+        }        
+        Start_New_Trial();
+
+      } else {
+        // Start the next move, if it is not reached               
+        setTimeout(function(){
+          Flash_Background_Correct();            
+          $('.processed').removeClass('processed')
+          $('.active').addClass('processed')      
+          Start_New_Move(); 
+        }, time_limit);
+      }      
   }
 
   $(document).keydown(function(e){   
     switch (e.which){    
-    case 37:    //left arrow key       
-        setTimeout(function(){
-        var [x_pos, y_pos] = Get_Position()
-        if (x_pos == 1) {
-          x_pos += 0;
-        } else {
-          x_pos -= 1;
-        }           
+    case 37:    //left arrow key      
+      //var [x_pos, y_pos] = Get_Position();
+      if (x_pos == 1) {
+        x_pos += 0;
+      } else {
+        x_pos -= 1;
+      }       
+      setTimeout(function(){      
+        $('.active').removeClass('active')
         $('[data-x=' + x_pos + '][data-y='+y_pos +']').addClass('active')                
-      },500);
-      Submit_Response('left');
-      break;
-    case 38:    //up arrow key
-        setTimeout(function(){
-        var [x_pos, y_pos] = Get_Position()
-        if (y_pos == 4) {
-          y_pos += 0;
-        } else {
-          y_pos += 1;
-        }        
-        $('[data-x=' + x_pos + '][data-y='+y_pos +']').addClass('active');
-      },500);
-      Submit_Response('up');
-      break;
-    case 39:    //right arrow key        
-        setTimeout(function(){
-        var [x_pos, y_pos] = Get_Position()
-        if (x_pos == 7) {
-          x_pos += 0;
-        } else {
-          x_pos += 1;
-        }    
-        $('[data-x=' + x_pos + '][data-y='+y_pos +']').addClass('active');
-        Submit_Response('right');
-      }, 500);
-      break;
-    case 40:    //bottom arrow key
+      },100);
+
+      var [new_x_pos, new_y_pos] = Get_Position();      
+
+      if ((Math.abs(new_x_pos - x_pos)) < 2){
+        Submit_Response('left');
+        break;
+      } else {
+        break;
+      }            
+      
+
+    case 38:    //up arrow key    
+      if (y_pos == 4) {
+        y_pos += 0;
+      } else {
+        y_pos += 1;
+      }          
       setTimeout(function(){
-        var [x_pos, y_pos] = Get_Position()
-        if (y_pos == 1) {
-          y_pos += 0;
-        } else {
-          y_pos -= 1;
-        }    
+        $('.active').removeClass('active')
+        $('[data-x=' + x_pos + '][data-y='+y_pos +']').addClass('active');
+      },100);
+
+      var [new_x_pos, new_y_pos] = Get_Position();
+
+      if ((Math.abs(new_y_pos - y_pos)) < 2){
+        Submit_Response('up');
+        break;
+      } else {
+        break;
+      }       
+           
+    case 39:    //right arrow key              
+      if (x_pos == 7) {
+        x_pos += 0;
+      } else {
+        x_pos += 1;
+      }        
+      setTimeout(function(){
+        $('.active').removeClass('active')
+        $('[data-x=' + x_pos + '][data-y='+y_pos +']').addClass('active');
+      }, 100);      
+      
+      var [new_x_pos, new_y_pos] = Get_Position();
+      if ((Math.abs(new_x_pos - x_pos)) < 2){
+        Submit_Response('right');
+        break;
+      } else {
+        break;
+      }       
+
+    case 40:    //down arrow key      
+      if (y_pos == 1) {
+        y_pos += 0;
+      } else {
+        y_pos -= 1;
+      }  
+      setTimeout(function(){  
+        $('.active').removeClass('active')
         $('[data-x=' + x_pos + '][data-y='+y_pos +']').addClass('active');                
-      },500);
-      Submit_Response('bottom');
-      break;
+      },100);
+
+      var [new_x_pos, new_y_pos] = Get_Position();
+      if ((Math.abs(new_y_pos - y_pos)) < 2){
+        Submit_Response('down');
+        break;
+      } else {
+        break;
+      }       
     }
   });
 });
