fe999405c6e30bea49f9a34f41cfd973708c8539
mehmetyoruten <mehmetyorutenn@gmail.com>
2022-02-10 17:14:15 +0100

    new backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { SessionsModule } from './sessions/sessions.module';
 import { ConfigModule } from './config/config.module';
 
-
 @Module({
   imports: [SessionsModule, ConfigModule],
   controllers: [AppController],
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Building API for Navigation Exp!';
+    return 'Hello World!';
   }
 }
