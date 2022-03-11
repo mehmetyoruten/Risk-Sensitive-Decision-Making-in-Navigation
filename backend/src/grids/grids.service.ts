6b0962e1a6551c07773380809216aa94738f3081
mehmetyoruten <mehmetyorutenn@gmail.com>
2022-03-11 14:06:38 +0100

    update grid info

--- backend/src/grids/grids.service.ts
+++ backend/src/grids/grids.service.ts
@@ -3,7 +3,7 @@ import { Grid } from './grids.model'
 
 @Injectable()
 export class GridService {
-private grids: Grid[] = [];
+  private grids: Grid[] = [];
 
   // try to call the grid with its Id
   async readGrid(){
@@ -30,6 +30,29 @@ private grids: Grid[] = [];
         throw new NotFoundException('Could not find grid.');
     }
     return {...grid}; 
+  }
+
+  updateGrid(gridId: string, gridWorld: number, endLoc: number, startLoc: number, player: number, obstacleLoc: number) {
+    const [grid, index] = this.findGrid(gridId);
+
+    const updatedGrid = {...grid};
+
+    if (gridWorld) {
+      updatedGrid.gridWorld = gridWorld;
+    }
+    if (endLoc) {
+      updatedGrid.endLoc = endLoc;      
+    }
+    if (startLoc) {
+      updatedGrid.startLoc = startLoc;      
+    } 
+    if (player) {
+      updatedGrid.player = player;      
+    }
+    if (obstacleLoc) {
+      updatedGrid.obstacleLoc = obstacleLoc;
+    }
+    this.grids[index] = updatedGrid;
   }
 
   private findGrid(id: string): [Grid, number] { 
